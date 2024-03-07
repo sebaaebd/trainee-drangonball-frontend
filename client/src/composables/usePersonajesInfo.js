5cd2a6ed6805496eddd2e2ed6dda22eecf2fea3e
sebaaebd <sbarria@apprende.cl>
2024-03-07 11:24:01 -0300

    fix/sbarria/1/update-conexion-api

--- client/src/composables/usePersonajesInfo.js
+++ client/src/composables/usePersonajesInfo.js
@@ -1,5 +1,5 @@
 
-import axios from 'axios';
+/* import axios from 'axios';
 
 // Define la función para obtener al personajes
 export const  getPersonajes = async  () => {
@@ -8,6 +8,30 @@ export const  getPersonajes = async  () => {
     const response = await axios.get('http://pruebaapi.myddns.me/public/personajes');
 
     const data = response.data
+
+    // Devuelve los datos de la respuesta
+    console.log('Datos recibidos:', data);
+    return data;
+  } catch (error) {
+    // Captura cualquier error que ocurra durante la solicitud y muestra un mensaje en la consola
+    console.error('Error al obtener los personajes:', error);
+    // Retorna el error
+    return error;
+  }
+} **/
+
+// Define la función para obtener al personajes
+export const getPersonajes = async () => {
+  try {
+    // Realiza la solicitud GET al endpoint
+    const response = await fetch('http://pruebaapi.myddns.me/public/personajes');
+
+    // Verifica si la respuesta es exitosa
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
 
     // Devuelve los datos de la respuesta
     console.log('Datos recibidos:', data);
