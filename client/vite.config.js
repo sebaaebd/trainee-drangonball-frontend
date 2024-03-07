c3adae2fafda2690377fe5e0a234e08422882250
sebaaebd <sbarria@apprende.cl>
2024-03-07 15:33:21 -0300

    fix/sbarria/3/update-web-viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),],
-  base: 'https://sebaaebd.github.io/trainee-drangonball-frontend/tree/main/client',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
